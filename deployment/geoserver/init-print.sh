797aaeaa19170d1a2101a7aa6079ae2f11a5be33
juanique <juanique@kartoza.com>
2023-07-21 08:21:44 +0200

    update docker

--- deployment/geoserver/init-print.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -e
-cp -r  /geoserver_data/data/printing/config/WAPP_logo.png /usr/local/tomcat/webapps/geoserver/data/printing/WAPP_logo.png
-cp -r  /geoserver_data/data/printing/config/config.yaml /usr/local/tomcat/webapps/geoserver/data/printing/config.yaml
-# Run the CMD 
-exec "$@"
